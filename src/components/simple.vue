613f955edb995117df8989a9c8cba165a0c16fb5
William <1072231718@qq.com>
2020-12-18 17:38:52 +0800

    单文件引入vue中写法

---
+++ src/components/simple.vue
@@ -0,0 +1,29 @@
+<script setup>
+  import counter from '../methods/useCounter'
+  import useMove from '../methods/useMove'
+
+  let {count, add, double} = counter();
+  let {x, double: doubleX} = useMove();
+
+  //第二种定义方式
+  ref: simple = 0;
+  function show() {
+    simple++;
+    console.log(simple);
+  }
+
+</script>
+
+<template>
+  <div>
+    <p>数值：{{count}}</p>
+    <p>double：{{double}}</p>
+    <p>移动显示：{{doubleX}}</p>
+    <button @click="add">累加</button>
+    <button @click="show">simple</button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
